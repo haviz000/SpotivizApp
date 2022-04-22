b04d5eebefea7e3c96f39f5343f177ded7e515d9
haviz000 <haviz.tasmara000@gmail.com>
2022-04-22 13:37:37 +0700

    redirect production

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const config = {
     SPOTIFY_BASE_URL: 'https://api.spotify.com/v1',
     SPOTIFY_SCOPE: 'playlist-modify-private',
     RESPONSE_TYPE: "token",
-    REDIRECT_URI: "http://localhost:3000",
+    REDIRECT_URI: "https://spotiviz.vercel.app/",
 }
 
 export default config;
